c9e424f3c36ab3286fb76794ddf859a561cf295e
ryanapreala <66791065+ryanapreala@users.noreply.github.com>
2023-01-26 19:40:19 +0000

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import { fetchRecommendedBookings } from '../actions/bookingActions';
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
-import styles from "./HomePage.module.css";
+import "./HomePage.module.css";
 
 /**
  * Homepage component that displays the booking categories and 4 recommendations per category.
@@ -20,24 +20,33 @@ const Home = ({ recommendedBookings, fetchRecommendedBookings }) => {
    * @param {Object} bookings - The recommended bookings object
    * @returns {JSX.Element} - JSX element of the booking categories and recommendations
    */
-  const renderBookingCategories = (bookings) => {
-    return Object.keys(bookings).map((category) => (
-      <div key={category}>
-        <h2>{category}</h2>
+
+  const renderBookingCategories = (bookings) =>
+    Object.keys(bookings).map((category) => (
+      <div key={category} className="category-container">
+        <h2 className="category-title">{category}</h2>
         {bookings[category].slice(0, 4).map((booking) => (
-          <div key={booking.id}>
-            <h3>{booking.name}</h3>
-            <img src={booking.imageUrl} alt={booking.name} />
-            <p>Price: {booking.price}</p>
-            <p>Location: {booking.location}</p>
+          <div key={booking.id} className="booking-container">
+            <h3 className="booking-name">{booking.name}</h3>
+            <img
+              src={booking.imageUrl}
+              alt={booking.name}
+              className="booking-image"
+            />
+            <p className="booking-price">Price: {booking.price}</p>
+            <p className="booking-location">Location: {booking.location}</p>
           </div>
         ))}
-        <button onClick={() => handleViewMore(category)}>View More</button>
+        <button
+          className="view-more-button"
+          onClick={() => handleViewMore(category)}
+        >
+          View More
+        </button>
       </div>
     ));
-  };
 
-  /**
+/**
    * Handles the view more button click
    * @param {string} category - The category of the bookings to view more of
    */
@@ -46,25 +55,17 @@ const Home = ({ recommendedBookings, fetchRecommendedBookings }) => {
   };
 
   return (
-    <>
+    <div className="home-container">
       <Header />
       <Hero />
-      <div>
-        <h1>Recommended Bookings</h1>
-        {renderBookingCategories(recommendedBookings)}
-      </div>
+      {renderBookingCategories(props.recommendedBookings)}
       <Footer />
-    </>
+    </div>
   );
 };
 
 const mapStateToProps = (state) => ({
-  recommendedBookings: state.bookings.recommended,
+  recommendedBookings: state.bookings.recommendedBookings,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchRecommendedBookings: () => dispatch(fetchRecommendedBookings()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
-
+export default connect(mapStateToProps, { fetchRecommendedBookings })(Home);
