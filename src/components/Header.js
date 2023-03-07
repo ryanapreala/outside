52ac93b121d57ec2f880160f23e8213236954863
Ryan Apreala <r.apreala@alustudent.com>
2023-03-07 02:33:43 +0000

    components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,59 +2,43 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <div className={styles.outside}>outside</div>
-      <div className={styles.travelDatesParent}>
-        <div className={styles.travelDates}>
-          <div className={styles.travelDatesChild} />
-          <div className={styles.travelDatesItem} />
-          <div className={styles.travelDatesInner}>
-            <div className={styles.searchParent}>
-              <b className={styles.search}>Search</b>
-              <img
-                className={styles.iconlycurvedsearch}
-                alt=""
-                src="../iconlycurvedsearch.svg"
-              />
-            </div>
-          </div>
-          <div className={styles.vuesaxoutlinelocationParent}>
-            <img
-              className={styles.vuesaxoutlinelocationIcon}
-              alt=""
-              src="../vuesaxoutlinelocation.svg"
-            />
-            <div className={styles.location}>Location</div>
-            <div className={styles.addLocation}>Add location</div>
-          </div>
-          <div className={styles.vuesaxlinearcalendarParent}>
-            <img
-              className={styles.vuesaxlinearcalendarIcon}
-              alt=""
-              src="../vuesaxlinearcalendar.svg"
-            />
-            <div className={styles.date}>Date</div>
-            <div className={styles.addDates}>Add dates</div>
-          </div>
-          <div className={styles.vuesaxlinearprofileParent}>
-            <img
-              className={styles.vuesaxlinearprofileIcon}
-              alt=""
-              src="../vuesaxlinearprofile.svg"
-            />
-            <div className={styles.guests}>Guests</div>
-            <div className={styles.addGuests}>Add guests</div>
-          </div>
-          <img className={styles.vectorIcon} alt="" src="../vector-1.svg" />
-          <img
-            className={styles.travelDatesChild1}
-            alt=""
-            src="../vector-2.svg"
-          />
-        </div>
-        <img className={styles.frameChild} alt="" src="../group-11.svg" />
+    <header className="header">
+      <div>
+        <h1>
+          <a className="logo" href="App.js">outside</a>
+        </h1>
       </div>
-    </div>
+      <div>
+        <div>
+          <img src="/public/location.svg" alt="Location icon"></img>
+          <div class="center content-inputs">
+          <vs-input border color="#D06224" type="Location" v-model="value" placeholder="Add Location" />
+          </div>
+        </div>
+        <div>
+          <img src="/public/calendar.svg" alt="Calendar icon"></img>
+          <div class="center content-inputs">
+          <vs-input border color="#D06224" type="Date" v-model="value" placeholder="Add Date" />
+          </div>
+        </div>
+        <div>
+          <img src="/public/profile.svg" alt="Profile icon"></img>
+          <div class="center content-inputs">
+          <vs-input border color="#D06224" type="Guests" v-model="value" placeholder="Add Guests" />
+          </div>
+        </div>
+        <div>
+          <img src="/public/search-normal.svg" alt="Search icon"></img>
+          <h2>Search</h2>
+        </div>
+        
+      </div>
+      <div>
+        <img src="/public/arrow-down.svg" alt="dropdown arrow"></img>
+        <img src="/public/profile-circle.svg" alt="profile"></img>
+      </div>
+      </header>
+
   );
 };
 
