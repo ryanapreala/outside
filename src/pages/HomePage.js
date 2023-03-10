d98937795ee912233faf5eb5d669e5e6fe1354c9
Sani Rogers <sanirogers138@gmail.com>
2023-03-10 12:20:19 -0500

    test

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import {Button} from 'react-bootstrap';
+import NavigationBar from '../components/NavigationBar';
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavigationBar />
+        </div>
+    );
+}
+
+export default HomePage;
