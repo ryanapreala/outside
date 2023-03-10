900988d1363541b3b982a9a3caea9fb89b56266a
Sani Rogers <sanirogers138@gmail.com>
2023-03-10 11:02:48 -0500

    test version

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import {Button} from 'react-bootstrap';
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <Button variant="primary">Primary</Button>
+        </div>
+    );
+}
+
+export default Home;
