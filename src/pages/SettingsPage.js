5ed7d71bc5e50dee3d6f22157fa1329e4f59a2c1
Sani Rogers <sanirogers138@gmail.com>
2023-06-30 13:51:41 -0400

    add icons and change text

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,6 +1,11 @@
 import styled from "@emotion/styled";
 import SettingCard from "../components/SettingCard";
-import { FaRegUserCircle } from "react-icons/fa";
+import { FaRegBell, FaRegCreditCard, FaRegQuestionCircle, FaRegUserCircle } from "react-icons/fa";
+import { FaEarthAfrica } from "react-icons/fa6";
+import { RiShieldKeyholeLine } from "react-icons/ri"
+import { HiOutlineBriefcase, HiOutlineGift } from "react-icons/hi";
+import { BsShieldX } from "react-icons/bs";
+
 
 const SettingsContainer = styled("div")({
     margin: 64,
@@ -32,44 +37,44 @@ const SettingsPage = () => {
                     <FaRegUserCircle color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Login & Security" 
+                settingDescription="Update your password and secure your account">
+                    <RiShieldKeyholeLine color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Payment Methods" 
+                settingDescription="Review payments, payouts, coupons, gift cards and taxes">
+                    <FaRegCreditCard color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Notifications" 
+                settingDescription="Choose notification preferences and how you want to be contacted">
+                    <FaRegBell color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Privacy and Sharing" 
+                settingDescription="Control connected apps, what you share and who sees it">
+                    <BsShieldX color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Global Preferences" 
+                settingDescription="Set your default language, currency, and timezone">
+                    <FaEarthAfrica color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Business" 
+                settingDescription="Add a work email for trip benefits">
+                    <HiOutlineBriefcase color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Get Help" 
+                settingDescription="Contact support for any issues">
+                    <FaRegQuestionCircle color="6f6e6e" size={56}/>
                 </SettingCard>
                 <SettingCard 
-                settingName="Personal Information" 
-                settingDescription="Provide personal details and how we can reach you">
-                    <FaRegUserCircle color="6f6e6e" size={56}/>
+                settingName="Referrals & Credits" 
+                settingDescription="You have $0 referral credits and coupons. Learn more.">
+                    <HiOutlineGift color="6f6e6e" size={56}/>
                 </SettingCard>
             </SettingsGrid>
         </SettingsContainer>
